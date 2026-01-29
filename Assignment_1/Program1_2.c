6128aad7211280644a3c872cd51063477cf77d4c
Pravin Chavan <pravinac8@gmail.com>
2026-01-29 13:48:31 +0530

    Update Program1_2.c

--- Assignment_1/Program1_2.c
+++ Assignment_1/Program1_2.c
@@ -6,7 +6,7 @@ void Display()
     int i =0;
     for(i=1;i<=5;i++)
     {
-        printf("Marvellous\n");
+        printf("Jay Ganesh\n");
     }
 }
 
@@ -18,3 +18,4 @@ int main()
 
     return 0;
 }
+
