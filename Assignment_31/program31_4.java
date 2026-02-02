b5fc49db393c5db24efcbeb70114a28b99756c2e
Pravin Chavan <pravinac8@gmail.com>
2026-02-02 14:28:42 +0530

    Add files via upload

---
+++ Assignment_31/program31_4.java
@@ -0,0 +1,66 @@
+/*
+Input : iRow : 4
+        iCol : 4
+Output :
+
+*       *       *       *
+*       *               *
+*               *       *
+*       *       *       *
+
+*/
+
+import java.util.Scanner;
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {   
+        int i=0,j=0;
+        if(iRow!=iCol)
+        {
+            System.out.println("Invalid Input");
+            System.out.println("The No. of rows and columns must be same");
+            return;
+        }
+        for(i=1;i<=iRow;i++)
+        {
+            for(j=1;j<=iCol;j++)
+            {   
+                if(i==j || i ==1 || j==1 || i == iRow || j==iCol )
+                {
+                System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+class program31_4
+{   
+    public static void main(String[] args) 
+    {   
+        int iValue1 =0;
+        int iValue2 =0;
+
+        Scanner sobj=new Scanner(System.in);
+
+        System.out.println("Enter the no. of Rows:");
+        iValue1= sobj.nextInt();
+
+        System.out.println("Enter the no. of Rows:");
+        iValue2= sobj.nextInt();
+
+        Pattern pobj =new Pattern();
+
+        pobj.Display(iValue1,iValue2);
+
+        sobj=null;
+        pobj=null;
+
+        
+    }
+}
